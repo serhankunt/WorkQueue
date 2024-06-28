e0b2ee84c27fe8ec11d8ee07c990a4d1ecf86764
serhankunt <hserhan006@gmail.com>
2024-06-28 21:11:11 +0300

    Add project files.

---
+++ Publisher/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+channel.QueueDeclare("hello-queue1", false, false, false);
+
+channel.QueueBind("hello-queue1", exchange: "logs", routingKey: string.Empty);
+
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    var message = GetMessage(args) + x;
+
+    #region RabbitMQ'da fiziksel kayıt için
+    //IBasicProperties properties = channel.CreateBasicProperties();
+    //properties.Persistent = true;
+    #endregion
+
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(
+        exchange: "logs",
+        routingKey: string.Empty,
+        basicProperties: null,
+        body: body);
+
+
+    Console.WriteLine($"[x] Sent {message}");
+    Console.WriteLine("Press [enter] to exit");
+});
+
+//int i = 0;
+//while (i < 100)
+//{
+//    var message = GetMessage(args) + i;
+//    var body = Encoding.UTF8.GetBytes(message);
+//    channel.BasicPublish(
+//    exchange: "logs",
+//    routingKey: string.Empty,
+//    basicProperties: null,
+//    body: body);
+//    i++;
+
+//    Console.WriteLine($"[x] Sent {message}");
+//    Console.WriteLine("Press [enter] to exit");
+//}
+Console.ReadLine();
+
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "info: Göt Ahmet!");
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+channel.QueueDeclare("hello-queue1", false, false, false);
+channel.BasicQos(prefetchSize: 0, prefetchCount: 5, global: false);
+//prefetchSize Mesaj boyutunu belirtir.0 değeri mesajın boyutuyla ilgilenmediğimizi belirtir.
+//prefetchCount mesaj dağıtım adetini ifade eder.Kaç kaç gelsin.
+//global: true=> tüm consumerların aynı anda prefetchCount parametresinde belirtilen değer kadar mesaj tüketebileceğini ifade eder.6mesaj 3 subscriber var ise 2şer şekilde dağıtımı gerçekleştiriyor.
+//        false=>her bir consumer'ın işleme sürecinde diğer consumerlardan bağımsız bir şekilde kaç mesaj alacağı belirtilir.
+
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(
+    queue: queueName,
+    exchange: "logs",
+    routingKey: string.Empty);
+
+Console.WriteLine("[*] waiting for logs");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    Thread.Sleep(1000);
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[x] {message}");
+    Console.WriteLine(ea.DeliveryTag);
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+    //ea.DeliveryTag consumer'ın aldığı her mesaj için eşsiz bir tanımlayıcı görevi gören bir değerdir.BasisAck metodu ile mesajı onaylarken bu değer kullanılır.
+    //multiple değeri false=> sadece tek bir mesajı onaylayacağını belirtir.ea.DeliveryTag parametresine hangi değeri verirseniz verin sadece o değere sahip mesaj onaylanacaktır.
+    //                true=> ea.DeliveryTag parametresine kadar gelen tüm mesajlar onaylanır.
+
+};
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: false,
+    consumer: consumer);
+
+Console.WriteLine("Press [enter] to exit");
+
+Console.ReadLine();
