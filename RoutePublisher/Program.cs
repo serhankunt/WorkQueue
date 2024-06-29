1ca35f281dca6ba2e2ba8e75cc1345f1c3719c7e
serhankunt <hserhan006@gmail.com>
2024-06-29 14:31:20 +0300

    .

---
+++ RoutePublisher/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+channel.QueueDeclare("deneme", false, false, false, arguments: null);
+
+
+int i = 0;
+string[] denemeList = ["deneme1", "deneme2"];
+var severity = "";
+while (i < 100)
+{
+    if (i % 2 == 0)
+    {
+        severity = denemeList[0];
+    }
+    else
+    {
+        severity = denemeList[1];
+    }
+
+    channel.QueueBind("deneme", exchange: "direct_logs", routingKey: severity);
+
+    var message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Göt Ahmet" + i;
+    i++;
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "direct_logs",
+        routingKey: severity,
+        basicProperties: null,
+        body: body);
+
+    Console.WriteLine($" [x] Sent '{severity} :'{message}'");
+    Console.WriteLine("Press [enter] to exit");
+}
+Console.ReadLine();
---
+++ RouteSubscriber/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+channel.QueueDeclare("deneme", false, false, false, arguments: null);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+string[] denemeList = ["deneme1", "deneme2"];
+
+
+foreach (var severity in denemeList)
+{
+    channel.QueueBind(queue: queueName,
+        exchange: "direct_logs",
+        routingKey: severity);
+}
+
+Console.WriteLine(" [*] Waiting for messages");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    Thread.Sleep(1000);
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var routingKey = ea.RoutingKey;
+    Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
+
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+channel.BasicConsume(queue: queueName,
+                    autoAck: false,
+                    consumer: consumer);
+
+Console.WriteLine("Press [enter] to exit");
+Console.ReadLine();
