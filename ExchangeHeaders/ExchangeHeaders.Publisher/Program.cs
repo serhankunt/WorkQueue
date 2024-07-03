917025651c1f7574f0c469b34fb8c7e0ba805799
serhankunt <hserhan006@gmail.com>
2024-07-03 14:28:35 +0300

    Headers exchange eklendi.

---
+++ ExchangeHeaders/ExchangeHeaders.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+
+
+Dictionary<string, object> headers = new Dictionary<string, object>();
+
+headers.Add("format", "pdf");
+headers.Add("shape", "a4");
+
+var properties = channel.CreateBasicProperties();
+properties.Headers = headers;
+
+channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes("header mesajım"));
+
+Console.WriteLine("mesaj gönderilmiştir.");
+
+Console.ReadLine();
---
+++ ExchangeHeaders/ExchangeHeaders.Subscriber/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+Dictionary<string, object> headers = new Dictionary<string, object>();
+
+headers.Add("format", "pdf");
+headers.Add("shape", "a4");
+headers.Add("x-match", "all");
+
+channel.QueueBind(queueName, "header-exchange", String.Empty, headers);
+
+channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine("Loglar dinleniyor");
+
+consumer.Received += (model, ea) =>
+{
+    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+    Console.WriteLine("Gelen Mesaj :" + message);
+    channel.BasicAck(ea.DeliveryTag, false);
+};
+
+Console.ReadLine();
